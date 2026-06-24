3f3369b58e0924a8f9b420e43fd326aa4b29fc5e
ABCDarioGitHub_24 <liberzi777@gmail.com>
2026-06-23 23:18:04 -0500

    feat: Implementar Cliente Controller.

---
+++ src/main/java/com/devliyez/asignarcarga/controller/ClienteController.java
@@ -0,0 +1,44 @@
+package com.devliyez.asignarcarga.controller;
+
+import com.devliyez.asignarcarga.dto.ClienteRegistrar;
+import com.devliyez.asignarcarga.dto.ClienteRequest;
+import com.devliyez.asignarcarga.dto.ClienteResponse;
+import com.devliyez.asignarcarga.services.Cliente.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cliente")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public List<ClienteResponse> getClientes(){
+        return clienteService.getClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ClienteResponse getClienteById(@PathVariable Long id){
+        return clienteService.getClienteById(id);
+    }
+
+    @PostMapping("/crear")
+    public ClienteResponse postCliente(@RequestBody ClienteRegistrar c){
+
+        return clienteService.postCliente(c);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ClienteResponse updateCliente(@RequestBody ClienteRequest c, @PathVariable Long id){
+        return clienteService.updateCliente(c,id);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void deleteCliente(@PathVariable Long id){
+        clienteService.deleteCliente(id);
+    }
+}
